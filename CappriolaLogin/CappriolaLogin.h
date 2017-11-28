a911e29f0d30f31af6ff385db17bca44735f8931
Dilermando Barbosa Jr <dilermando.barbosa@gmail.com>
2017-11-28 11:09:11 -0200

    Initial Commit

---
+++ CappriolaLogin/CappriolaLogin.h
@@ -0,0 +1,19 @@
+//
+//  CappriolaLogin.h
+//  CappriolaLogin
+//
+//  Created by Diler Barbosa on 28/11/17.
+//  Copyright © 2017 Diler Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CappriolaLogin.
+FOUNDATION_EXPORT double CappriolaLoginVersionNumber;
+
+//! Project version string for CappriolaLogin.
+FOUNDATION_EXPORT const unsigned char CappriolaLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CappriolaLogin/PublicHeader.h>
+
+
